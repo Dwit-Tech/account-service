e69f8e7ca35fdc04abbe070128da7233f9bcf011
Priscilla Okoro <oyomepriscilla@gmail.com>
2023-02-15 01:35:12 -0500

    create role entity

--- src/DwitTech.AccountService.Data/Entities/RoleEntity.cs
+++ src/DwitTech.AccountService.Data/Entities/RoleEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
-public class RoleDTO
+namespace DwitTech.AccountService.Data.Entities
+public class Role : BaseEntity
 {
     public int Id { get; set; }
     public string Name { get; set; }
