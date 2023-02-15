dae9473a809c2c5fb6b5d3214f25231f19c05067
Priscilla Okoro <oyomepriscilla@gmail.com>
2023-02-15 01:47:52 -0500

    create role repo interphase

---
+++ src/DwitTech.AccountService.Core/Interfaces/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace.DwitTech.AccountService.Core.Interfaces
+    public interface IRoleRepository 
+{
+    Task<Role> GetByIdAsync(int id);
+    Task<List<Role>> GetAllAsync();
+    Task AddAsync(Role role);
+    Task UpdateAsync(Role role);
+    Task DeleteAsync(Role role);
+}
+
