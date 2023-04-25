4f5187cf48eb79db4fc9313ebbf0b6b68f851f13
johnny2565 <masterj6525@gmail.com>
2023-04-25 19:18:04 +0100

    Fixed failing test to match current changes

--- tests/DwitTech.AccountService.Core.Tests/Middleware/AuthorizationMiddlewareTest.cs
+++ tests/DwitTech.AccountService.Core.Tests/Middleware/AuthorizationMiddlewareTest.cs
@@ -51,37 +51,53 @@ namespace DwitTech.AccountService.Core.Tests.Middleware
         }
 
         [Fact]
-        public async Task InvokeAsync_WithInvalidApiKey_ReturnsUnauthorized()
+        public async Task InvokeAsync_WithInvalidApiKey_ShouldReturnUnauthorized()
         {
-            // Arrange
-            var context = new DefaultHttpContext();
-            context.Request.Headers["X_API_KEY"] = "invalid-api-key";
-            context.Connection.RemoteIpAddress = IPAddress.Parse("127.0.0.1");
 
-            var middleware = new AuthorizationMiddleware(context => Task.CompletedTask, _configuration);
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "X_API_KEYS", "valid-key" }
+                })
+                .Build();
+
+            var middleware = new AuthorizationMiddleware(
+                (innerHttpContext) => Task.FromResult(0),
+                configuration);
+
+
+            var context = new DefaultHttpContext();
+            context.Request.Headers["X_API_KEY"] = "invalid-key";
+            context.Response.Body = new MemoryStream();
 
             // Act
             await middleware.InvokeAsync(context);
 
             // Assert
             Assert.Equal((int)HttpStatusCode.Unauthorized, context.Response.StatusCode);
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            Assert.Equal("Invalid API key", responseBody);
+
         }
 
         [Fact]
-        public async Task InvokeAsync_WithInvalidSourceIpAndApiKey_ReturnsUnauthorized()
+        public async Task InvokeAsync_WithPathContainingHealth_ShouldCallNextMiddleware()
         {
             // Arrange
+            var configuration = new Mock<IConfiguration>();
+            var middleware = new AuthorizationMiddleware(
+                (innerHttpContext) => Task.FromResult(0),
+                configuration.Object);
             var context = new DefaultHttpContext();
-            context.Request.Headers["X_API_KEY"] = "invalid-api-key";
-            context.Connection.RemoteIpAddress = IPAddress.Parse("192.168.1.3");
-
-            var middleware = new AuthorizationMiddleware(context => Task.CompletedTask, _configuration);
+            context.Request.Path = "/health";
 
             // Act
             await middleware.InvokeAsync(context);
 
             // Assert
-            Assert.Equal((int)HttpStatusCode.Unauthorized, context.Response.StatusCode);
+            Assert.Equal((int)HttpStatusCode.OK, context.Response.StatusCode);
+
         }
     }
 }
